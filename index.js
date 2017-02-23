cbdf142b7995936787b80cd48b9739a76906cc66
Daniel Son <sondaniel96@gmail.com>
2017-02-23 10:04:08 -0800

    added schedule. removed dependencies

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
-
-app.use('/static', express.static(path.join(__dirname, '/static')))
+app.use('/static', express.static(path.join(__dirname, '/static')));
+app.use('/scripts', express.static(path.join(__dirname, '/node_modules')));
 
 // viewed at http://localhost:8080
 app.get('/', function(req, res) {
