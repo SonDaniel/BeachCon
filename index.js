f75ad884585e08fe2ea21fa168100ff5f9838d39
Daniel Son <sondaniel96@gmail.com>
2017-03-14 17:21:04 -0700

    revert back to orginal. https is now on nginx

--- index.js
+++ index.js
@@ -5,9 +5,6 @@ var path = require('path');
 app.use('/static', express.static(path.join(__dirname, '/static')));
 app.use('/scripts', express.static(path.join(__dirname, '/node_modules')));
 
-app.get('*',function(req, res) {
-    res.redirect('https://beachcon.org'+req.url);
-});
 // viewed at http://localhost:8080
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
